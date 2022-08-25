80ccc5e9108c040956af3b850fedc8725663a77b
Palash <chadhapalash2000@gmail.com>
2022-08-25 20:08:37 +0400

    added port

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 app.set("view engine", "hbs");
 
+const PORT = process.env.PORT || 5000;
+
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log("server is running");
 });
